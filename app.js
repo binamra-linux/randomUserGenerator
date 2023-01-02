357f7af4c9aab8e7f710b2da827d95bd711a873d
binamra-linux <53526396+binamra-linux@users.noreply.github.com>
2023-01-02 17:10:39 +0545

    Update app.js

--- app.js
+++ app.js
@@ -9,13 +9,19 @@ async function users(){
 }
 
 function showUsers(info){
-	//console.log(info)
+	console.log(info)
 	let pic = info.picture.large
+	let [dobDate, dobTime] = info.dob.date.split("T")
 	document.getElementById("img").setAttribute("src", pic)
 	document.getElementById("name").innerHTML = `${info.name.title} ${info.name.first} ${info.name.last}`
 	document.getElementById("gender").innerText = info.gender
-	document.getElementById("phone").innerText = info.cell
-	document.getElementById("dob").innerText = info.dob.date
+	document.getElementById("phone").innerText = info.phone
+	document.getElementById("dob").innerText = dobDate
+	document.getElementById("country").innerText = info.location.country
+	document.getElementById("city").innerText = info.location.city
+	document.getElementById("state").innerText = info.location.state
+	document.getElementById("street").innerHTML = `${info.location.street.name} ${info.location.street.number}`
+	document.getElementById("postcode").innerText = info.location.postcode
 }
 
 let btn = document.getElementById("btn");
