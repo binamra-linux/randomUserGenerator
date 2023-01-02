311bf95dcc4c860121da5f72319752f0c8287155
binamra-linux <53526396+binamra-linux@users.noreply.github.com>
2023-01-02 14:48:57 +0545

    Add files via upload

---
+++ app.js
@@ -0,0 +1,27 @@
+async function users(){
+	let apiReq = fetch("https://randomuser.me/api");
+	let data = apiReq.then((value)=>{
+		return value.json()
+	}).then((info)=>{
+		let userInfoData = info.results[0]
+		showUsers(userInfoData);
+	})
+}
+
+function showUsers(info){
+	console.log(info)
+	let pic = info.picture.medium
+	document.getElementById("img").setAttribute("src", pic)
+	document.getElementById("name").innerHTML = `${info.name.title} ${info.name.first} ${info.name.last}`
+	document.getElementById("gender").innerText = info.gender
+	document.getElementById("phone").innerText = info.cell
+	document.getElementById("dob").innerText = info.dob.date
+}
+
+let btn = document.getElementById("btn");
+btn.addEventListener("click", ()=>{
+	location.reload()
+})
+
+users()
+
